7fff1a35db789a0f7e7a3d3f06dc023b07302f11
celestialphineas <hanghang0713@hotmail.com>
2018-07-18 16:45:22 +0800

    Gradient added

---
+++ huozi/components/GradientView.swift
@@ -0,0 +1,31 @@
+//
+//  GradientView.swift
+//  huozi
+//
+//  Created by Celestial Phineas on 2018/7/18.
+//  Copyright © 2018年 Celestial Phineas. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var color1   : UIColor = UIColor.white
+    @IBInspectable var color2   : UIColor = UIColor.init(red: 1, green: 1, blue: 1, alpha: 0)
+
+    override func draw(_ rect: CGRect) {
+        let ctx         = UIGraphicsGetCurrentContext()!
+        let colors      = [color1.cgColor, color2.cgColor]
+        let colorSpace  = CGColorSpaceCreateDeviceRGB()
+        let colorLocations: [CGFloat] = [0, 1]
+        let gradient    = CGGradient(colorsSpace    : colorSpace,
+                                     colors         : colors as CFArray,
+                                     locations      : colorLocations)!
+        let startPoint  = CGPoint.zero
+        let endPoint    = CGPoint(x: 0, y: bounds.height)
+        ctx.drawLinearGradient(gradient,
+                               start: startPoint,
+                               end: endPoint,
+                               options: [])
+    }
+}
