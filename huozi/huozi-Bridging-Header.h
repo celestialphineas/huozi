fce8bcfe563dd25063f3bec2689ceaad8fba711d
celestialphineas <hanghang0713@hotmail.com>
2018-07-23 14:23:04 +0800

    A working carousel

---
+++ huozi/huozi-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  huozi-Bridging-Header.h
+//  huozi
+//
+//  Created by Celestial Phineas on 2018/7/23.
+//  Copyright © 2018年 Celestial Phineas. All rights reserved.
+//
+
+#ifndef huozi_Bridging_Header_h
+#define huozi_Bridging_Header_h
+
+#import "iCarousel.h"
+
+#endif /* huozi_Bridging_Header_h */
---
+++ huozi/views/courseHome/CourseCarouselViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CourseCarouselViewController.swift
+//  huozi
+//
+//  Created by Celestial Phineas on 2018/7/23.
+//  Copyright © 2018年 Celestial Phineas. All rights reserved.
+//
+
+import Foundation
